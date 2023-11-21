7d1afc6a1f7cfa27ea6531648524630e5c372d7b
gds90 <you@example.com>
2023-11-21 16:42:20 +0100

    Inizio milestone 2

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 // MILESTONE 1
 
-// dichiaro una costante button selezionando il bottone in HTML con la classe .btn
-const button = document.querySelector('.btn');
+// dichiaro una costante button selezionando il bottone in HTML con la classe .calcola
+const button = document.querySelector('.calcola');
 
-// aggiungo una funzione al bottone tramite click
+// aggiungo una funzione al bottone "Calcola prezzo" tramite click
 button.addEventListener('click', function(){
-    // dichiaro una variabile km prendendo l'elemento input con id #km
+    // dichiaro una variabile km prendendo il valore dell'elemento input con id #km
     let km = document.getElementById('km').value;
-    
-    // dichiaro una variabile eta prendendo l'elemento input con id #eta
+    console.log(km)
+    // dichiaro una variabile eta prendendo il valore dell'elemento input con id #eta
     let eta = document.getElementById('eta').value;
     
     // calcolo il prezzo del biglietto in base ai km da percorrere
@@ -22,11 +22,24 @@ button.addEventListener('click', function(){
         // sconto del 40% per gli over 65
         prezzo_biglietto *= 0.6;
     }
+    
    
     // formatto il prezzo con max due decimali 
     prezzo_biglietto = prezzo_biglietto.toFixed(2);
 
     // stampo il prezzo finale con eventuale sconto in console
     console.log("Il prezzo del biglietto è di €" +  prezzo_biglietto);
+
+    // // MILESTONE 2
+
+    // // dichiaro una variabile nome prendendo il valore dell'elemento input con id #nome
+    // let nome = document.getElementById('nome').value;
+
+    // // mostro il nome del passegger a video in HTML
+    // document.getElementById('nome_passeggero').innerHTML = nome;
+
+    // // mostro il prezzo finale con eventuale sconto a video in HTML
+    // document.getElementById('prezzo_biglietto').innerHTML = ("Il prezzo del biglietto è di €" +  prezzo_biglietto);
 })
+
 
